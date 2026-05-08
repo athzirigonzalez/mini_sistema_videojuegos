7c9e73d950f7478f3590431f4f60ec1efc4e0904
athzirigonzalez <athziri.gonzalez9051@alumnos.udg.mx>
2026-05-08 03:46:28 -0600

    Update controllers.py

--- controllers.py
+++ controllers.py
@@ -1,37 +1,6 @@
-from models import db, Juego
+from flask import Blueprint, jsonify, request
+from models import db, Juego # Importación directa desde el mismo nivel
 
-# LISTAR TODOS LOS JUEGOS
-def obtener_juegos():
-    return Juego.query.all()
+api = Blueprint('api', __name__)
 
-
-# OBTENER UN JUEGO POR ID
-def obtener_juego(id):
-    return Juego.query.get(id)
-
-
-# AGREGAR JUEGO
-def agregar_juego(nombre, genero, precio):
-    nuevo_juego = Juego(
-        nombre=nombre,
-        genero=genero,
-        precio=precio
-    )
-    db.session.add(nuevo_juego)
-    db.session.commit()
-
-
-# ACTUALIZAR JUEGO
-def actualizar_juego(id, nombre, genero, precio):
-    juego = Juego.query.get(id)
-    juego.nombre = nombre
-    juego.genero = genero
-    juego.precio = precio
-    db.session.commit()
-
-
-# ELIMINAR JUEGO
-def eliminar_juego(id):
-    juego = Juego.query.get(id)
-    db.session.delete(juego)
-    db.session.commit()
+# Aquí sigue el resto de tus rutas (get, post, etc.)
